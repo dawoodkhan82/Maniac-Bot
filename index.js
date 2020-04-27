0a31b431bdb31415d7a8251deef46b699907bbf2
dawoodkhan82 <dawoodkhan82@gmail.com>
2020-04-27 07:40:14 -0700

    app updates

--- index.js
+++ index.js
@@ -2,18 +2,60 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
+
+const addComment = `
+    mutation comment($id: ID!, $body: String!) {
+      addComment(input: {subjectId: $id, body: $body}) {
+        clientMutationId
+      }
+    }
+  `
+
+const blame = `
+query blame($login: String!, $name: String!, $path: String!){
+  repositoryOwner(login: $login) {
+    repository(name: $name) {
+      object(expression: "master") {
+        ... on Commit {
+          blame(path: $path) {
+            ranges {
+              startingLine
+              endingLine
+              age
+              commit {
+                oid
+                authoredDate
+                author {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+  app.on('push', async context => {
+    // console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    const commits = context.payload.commits
+    var filesChanged = []
+    var i
+    for (i = 0; i < commits.length; i++) {
+      filesChanged = filesChanged.concat(commits[i]['modified'])
+    }
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+    for (i = 0; i < filesChanged.length; i++) {
+      const blameResponse = await context.github.graphql(blame, {
+        login: context.payload.repository.owner.name,
+        name: context.payload.repository.name,
+        path: filesChanged[i]
+      })
+      // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+      // console.log(blameResponse['repositoryOwner']['repository']['object']['blame']['ranges'][0]['commit'])
+    }
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
