97c5c83a49efd1ca2042c6d989c1077c92b95386
aliabd <ali.si3luwa@gmail.com>
2020-04-27 11:39:37 -0700

    refatored maniac, changed to match new api

---
+++ maniac.py
@@ -0,0 +1,200 @@
+import os
+from datetime import datetime
+import ast
+
+FLAGS_TEXT_PATH = 'flags.txt'
+TEST_FILE_PATH = 'test_file.py'
+
+NODE_TYPES = {
+    ast.ClassDef: 'Class',
+    ast.FunctionDef: 'Function/Method',
+    ast.AsyncFunctionDef: 'AsyncFunction/Method',
+}
+
+
+def get_line_numbers(source):
+    tree = ast.parse(source)
+    line_numbers = {}
+
+    for node in ast.walk(tree):
+        if isinstance(node, tuple(NODE_TYPES)):
+            name = getattr(node, 'name', None)
+            function_lineno = getattr(node, 'lineno', None)
+            doc_lineno_start, doc_lineno_end = None, None
+            code_lines = [body.lineno for body in node.body
+                          if not isinstance(body, ast.Expr)]
+            code_lineno_start, code_lineno_end = min(code_lines) - 1, \
+                                                 max(code_lines) - 1
+
+            if (node.body and isinstance(node.body[0], ast.Expr) and
+                    isinstance(node.body[0].value, ast.Str)):
+                doc_lineno_end = node.body[0].lineno - 1
+                doc_lineno_start = doc_lineno_end - len(node.body[
+                                                0].value.s.splitlines())
+            line_numbers[name] = {
+                "type": NODE_TYPES[(type(node))],
+                "function_lineno": function_lineno,
+                "doc_lineno_start": doc_lineno_start,
+                "doc_lineno_end": doc_lineno_end,
+                "code_lineno_start": code_lineno_start,
+                "code_lineno_end": code_lineno_end
+            }
+    return line_numbers
+
+
+def convert_to_datetime(string):
+    datetime_object = datetime.strptime(string, '%Y-%m-%dT%H:%M:%SZ')
+    return datetime_object
+
+
+def git_blame(fp):
+    stream = os.popen(f'git blame {fp}')
+    output = stream.readlines()
+    output = [x.strip() for x in output]
+    return output
+
+
+def get_dates(blame_output, lines, name):
+    if lines[name]["doc_lineno_start"]:
+        if lines[name]["doc_lineno_start"] == \
+                lines[name]["doc_lineno_end"]:
+            doc_lines = blame_output[lines[name]["doc_lineno_start"]]
+            doc_dates = doc_lines['date']
+            latest_doc_date = convert_to_datetime(doc_dates)
+            doc_index = None
+        else:
+            doc_lines = blame_output[lines[name]["doc_lineno_start"]:lines[
+                name]["doc_lineno_end"]]
+            doc_dates = [doc_line['date'] for doc_line in doc_lines]
+            doc_dates = [convert_to_datetime(t) for t in doc_dates]
+            latest_doc_date = max(doc_dates)
+            doc_index = doc_dates.index(latest_doc_date)
+    else:
+        latest_doc_date = None
+        doc_index = None
+
+    if lines[name]["code_lineno_start"] == lines[name]["code_lineno_end"]:
+        code_lines = blame_output[lines[name]["code_lineno_start"]]
+        code_dates = code_lines['date']
+        latest_code_date = convert_to_datetime(code_dates)
+        code_index = None
+    else:
+        code_lines = blame_output[lines[name]["code_lineno_start"]:lines[name][
+            "code_lineno_end"]]
+        code_dates = [code_line['date'] for code_line in code_lines]
+        code_output_dates = [convert_to_datetime(item) for item in
+                             code_dates]
+        latest_code_date = max(code_output_dates)
+        code_index = code_dates.index(latest_code_date)
+
+    return latest_doc_date, latest_code_date, doc_index, code_index
+
+
+def get_author(blame_output, lines, name, code_index):
+    if lines[name]["code_lineno_start"] == \
+            lines[name]["code_lineno_end"]:
+        author = blame_output[lines[name]["code_lineno_start"]]['author']
+    else:
+        author = blame_output[lines[name]["code_lineno_start"]:lines[name][
+                "code_lineno_end"]][code_index]['author']
+    return author
+
+
+def get_last_doc_commit(blame_output, lines, name, doc_index):
+    if lines[name]["doc_lineno_start"] == \
+            lines[name]["doc_lineno_end"]:
+        last_doc_commit = blame_output[lines[name]["doc_lineno_start"]][
+            'commit']
+    else:
+        last_doc_commit = blame_output[lines[name]["doc_lineno_start"]:lines[
+            name][
+                "doc_lineno_end"]][doc_index]['commit']
+    return last_doc_commit
+
+
+def run_flags(source):
+    lines = get_line_numbers(source)
+    blame_output = git_blame(TEST_FILE_PATH)
+
+    # output = [{
+    #     'line': blame_ranges['startingLine'],
+    #     'commit': blame_ranges['commit']['oid'],
+    #     'date': blame_ranges['commit']['authoredDate'],
+    #     'author': blame_ranges['commit']['name']
+    # },]
+
+    flags = {}
+    for name in lines.keys():
+        latest_doc_date, latest_code_date, doc_index, code_index = \
+            get_dates(blame_output, lines, name)
+
+        if not latest_doc_date:
+            missing = True
+            stale = True
+            time_behind = None
+            last_doc_commit = None
+            author = get_author(blame_output, lines, name, code_index)
+
+        elif latest_code_date > latest_doc_date:
+            stale = True
+            missing = False
+            time_behind = latest_code_date - latest_doc_date
+            last_doc_commit = get_last_doc_commit(blame_output, lines, name,
+                                                  doc_index)
+            author = get_author(blame_output, lines, name, code_index)
+
+        else:
+            stale = False
+            missing = False
+            time_behind = None
+            last_doc_commit = None
+            author = None
+
+        flags[name] = {
+            "is_stale": stale,
+            "is_missing": missing,
+            "time_behind": time_behind,
+            "last_doc_commit": last_doc_commit,
+            "author": author
+        }
+
+    return flags
+
+
+def save_flags():
+    with open(TEST_FILE_PATH) as fp:
+        source = fp.read()
+        flags = run_flags(source)
+        stale_docs, missing_docs, passed = [], [], []
+        for fn in flags.keys():
+            if flags[fn]["is_missing"]:
+                missing_docs.append(f"{fn} |"
+                                    f" CODE UPDATED BY: {author}")
+            elif flags[fn]["is_stale"]:
+                time_behind = flags[fn]["time_behind"]
+                last_doc_commit = flags[fn]["last_doc_commit"]
+                author = flags[fn]["author"]
+
+                stale_docs.append(f"{fn} | TIME BEHIND: "
+                            f"{time_behind} | LAST DOC COMMIT: "
+                            f"{last_doc_commit} "
+                            f"| CODE UPDATED BY: {author}")
+            else:
+                passed.append(f"{fn} ")
+
+    with open(FLAGS_TEXT_PATH, 'w') as f:
+        f.write("STALE DOCSTRINGS:\n")
+        f.write("-"*len("STALE DOCSTRINGS:") + "\n")
+        for item in stale_docs:
+            f.write("%s\n" % item)
+        f.write("\nMISSING DOCSTRINGS:\n")
+        f.write("-" * len("MISSING DOCSTRINGS:") + "\n")
+        for item in missing_docs:
+            f.write("%s\n" % item)
+        f.write("\nUP TO DATE:\n")
+        f.write("-" * len("UP TO DATE:") + "\n")
+        for item in passed:
+            f.write("%s\n" % item)
+
+
+save_flags()
--- test.py
+++ test.py
@@ -8,9 +8,50 @@ def read_in():
 
 def main():
     #get our data as an array from read_in()
-    blame_json = read_in()
-    print("blame json data: ", blame_json)
+    lines = read_in()
+    blame_ranges = lines['repositoryOwner']['repository']['object'][
+            'blame']['ranges']
+
+    content = []
+    for blame_range in blame_ranges:
+        if blame_range['startingLine'] == blame_range['endingLine']:
+            content.append({
+                'line': blame_range['startingLine'],
+                'commit': blame_range['commit']['oid'],
+                'date': blame_range['commit']['authoredDate'],
+                'author': blame_range['commit']['name']
+            })
+        else:
+            for i in range(blame_range['endingLine'] - blame_range[\
+                    'startingLine'] + 1):
+                content.append({
+                    'line': blame_range['startingLine'] + i,
+                    'commit': blame_range['commit']['oid'],
+                    'date': blame_range['commit']['authoredDate'],
+                    'author': blame_range['commit']['name']
+                })
+
+
+    # print("blame json data: ", lines)
 
 #start process
 if __name__ == '__main__':
     main()
+
+# {'repositoryOwner': {'repository': {'object': {'blame': {'ranges': [
+#     {'startingLine': 1, 'endingLine': 1, 'age': 1,
+#      'commit': {'oid': 'e040c58542952848e298611a67daded63b414680',
+#                 'authoredDate': '2020-04-27T15:31:40Z',
+#                 'author': {'name': 'dawoodkhan82'}}},
+#     {'startingLine': 2, 'endingLine': 2, 'age': 1,
+#      'commit': {'oid': '47042e9a5f05e7adb877afd37681c6ba72445f64',
+#                 'authoredDate': '2020-04-27T15:33:00Z',
+#                 'author': {'name': 'dawoodkhan82'}}},
+#     {'startingLine': 3, 'endingLine': 24, 'age': 1,
+#      'commit': {'oid': 'e040c58542952848e298611a67daded63b414680',
+#                 'authoredDate': '2020-04-27T15:31:40Z',
+#                 'author': {'name': 'dawoodkhan82'}}},
+#     {'startingLine': 25, 'endingLine': 25, 'age': 1,
+#      'commit': {'oid': 'a5aec8dc6dc3dae616661128045c0ab3147714fb',
+#                 'authoredDate': '2020-04-27T15:34:54Z',
+#                 'author': {'name': 'dawoodkhan82'}}}]}}}}}
