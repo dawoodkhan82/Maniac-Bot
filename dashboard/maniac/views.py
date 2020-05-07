7e6089e7839e2586a707766db5965bb8c24b0a74
aliabd <ali.si3luwa@gmail.com>
2020-05-07 02:38:10 -0700

    pass repo name in url

--- dashboard/maniac/views.py
+++ dashboard/maniac/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the maniac dashboard    index.")
+def index(request, repo_name):
+    return HttpResponse("Hello, world. You're at the maniac dashboard "
+                        f"for {repo_name}.")
 
 # Create your views here.
