95f5e2ddba023dbc6b95539ceb1f5800163391cc
aliabd <ali.si3luwa@gmail.com>
2020-05-09 02:23:38 -0700

    changed path to static

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_URL = '/maniac/static/'
+STATICFILES_DIRS = (os.path.join('static'), )
 
 
 import django_heroku
