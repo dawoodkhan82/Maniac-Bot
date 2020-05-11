7a9c333f3355f088961bc7e2a3761c672f3c4a14
aliabd <ali.si3luwa@gmail.com>
2020-05-11 00:06:29 -0700

    removed cors headers

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -38,11 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,10 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-
-CORS_ORIGIN_ALLOW_ALL = True
 
 
 
