964389668bd9607df9fa3154b5b1410be40a3d67
dawoodkhan82 <dawoodkhan82@gmail.com>
2020-05-15 08:04:10 -0700

    add pr support

--- app/index.js
+++ app/index.js
@@ -35,44 +35,42 @@ query blame($login: String!, $name: String!, $path: String!){
 
 module.exports = app => {
   app.on(['pull_request.opened', 'pull_request.edited'], async context => {
-    app.log(context.payload)
-  //   const commits = context.payload.commits
-  //   var filesChanged = []
-  //   var i
-  //   for (i = 0; i < commits.length; i++) {
-  //     filesChanged = filesChanged.concat(commits[i]['modified'])
-  //   }
-  //
-  //   for (i = 0; i < filesChanged.length; i++) {
-  //     const blameResponse = await context.github.graphql(blame, {
-  //       login: context.payload.repository.owner.name,
-  //       name: context.payload.repository.name,
-  //       path: filesChanged[i]
-  //     })
-  //
-  //     const params = context.repo({ path: filesChanged[i] })
-  //     const contents = await context.github.repos.getContents(params)
-  //
-  //     let pyshell = new PythonShell('funnel.py');
-  //     data = blameResponse,
-  //     dataString = '';
-  //
-  //     pyshell.send(contents['data']['download_url']);
-  //     pyshell.send(JSON.stringify(data));
-  //
-  //     pyshell.on('message', function (data) {
-  //       dataString += data.toString();
-  //       dataString = JSON.parse(dataString)
-  //     });
-  //
-  //     pyshell.end(function (err,code,signal) {
-  //       if (err) throw err;
-  //       const params = context.repo({ commit_sha: context.payload.head_commit.id, body: dataString['6'], path: filesChanged[i], position: 1, line: 1 })
-  //       context.github.repos.createCommitComment(params)
-  //       console.log('The exit code was: ' + code);
-  //       console.log('The exit signal was: ' + signal);
-  //       console.log('finished');
-  //     });
-  //   }
+    const params = context.repo({pull_number: context.payload.pull_request.number})
+    const filesChanged = await context.github.pulls.listFiles(params)
+
+    var i
+    for (i = 0; i < filesChanged['data'].length; i++) {
+      const blameResponse = await context.github.graphql(blame, {
+        login: context.payload.repository.owner.login,
+        name: context.payload.repository.name,
+        path: filesChanged['data'][i]['filename']
+      })
+
+      let pyshell = new PythonShell('funnel.py');
+      data = blameResponse,
+      dataString = '';
+
+      pyshell.send(context.payload.repository.name)
+      pyshell.send(filesChanged['data'][i]['raw_url']);
+      pyshell.send(JSON.stringify(data));
+
+      pyshell.on('message', function (data) {
+        dataString += data.toString();
+        dataString = JSON.parse(dataString)
+      });
+      app.log(dataString)
+      pyshell.end(function (err,code,signal) {
+        if (err) throw err;
+        const params = context.repo({ pull_number:
+          context.payload.pull_request.number,
+          commit_id: filesChanged['data'][i]['sha'],
+          path: filesChanged['data'][i]['filename'], start_line: 1,
+          start_side: 'LEFT', line: 3, side: 'RIGHT', body: dataString })
+        context.github.pulls.createComment(params)
+        console.log('The exit code was: ' + code);
+        console.log('The exit signal was: ' + signal);
+        console.log('finished');
+      });
+    }
   })
 }
